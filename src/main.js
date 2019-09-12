10187d461f1da7c000f45c1d4f567dd0da6719a8
jumper0419 <18780243212@163.com>
2019-09-12 11:38:25 +0800

    代码提交

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/font-awesome-4.7.0/css/font-awesome.min.css'
 
 Vue.config.productionTip = false
-
 
 new Vue({
   router,
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,53 +1,19 @@
 <template>
   <div id="home">
-    <el-container style="height:100%;" direction="vertical">
-      <el-header>Header</el-header>
-      <el-container>
-        <el-aside width="200px">Aside</el-aside>
-        <el-container>
-          <el-main>Main</el-main>
-          <el-footer>Footer</el-footer>
-        </el-container>
-      </el-container>
-    </el-container>
+    <h2>首页</h2>
   </div>
 </template>
 
 <script>
+  import Layout from "components/content/layout/Layout";
+
   export default {
-    name: "Home"
+    name: "Home",
+    components: {
+      Layout
+    }
   }
 </script>
 
 <style scoped>
-  html,
-  body,
-  #app,
-  #home,
-  .el-container {
-    padding: 0px;
-    margin: 0px;
-    height: 100%;
-  }
-
-  .el-header, .el-footer {
-    background-color: #242f42;
-    color: #fff;
-    text-align: center;
-    line-height: 60px;
-  }
-
-  .el-aside {
-    background-color: #242f42;
-    color: #fff;
-    text-align: center;
-    line-height: 60px;
-  }
-
-  .el-main {
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-  }
-
 </style>
